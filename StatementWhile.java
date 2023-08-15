984308fc2f201e7213faf9e8d956b974602978e8
木村駿也 <kimurashunya@kimurashunyanoMacBook-Pro.local>
2023-08-15 22:26:25 +0900

    Add StatementWhile

---
+++ StatementWhile.java
@@ -0,0 +1,14 @@
+public class StatementWhile{
+ public static void main(String[]args){
+  //変数iに0を代入
+  int i=0;
+
+  //iが4以下であれば繰り返す
+  while(i <= 4){
+   //挨拶文を表示
+   System.out.println("こんにちは!");
+   //変数iをインクリメント
+   i++;
+  }
+ }
+}
