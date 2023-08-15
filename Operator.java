1ce6c7c735282dc2fbd0b3a9903d072a6e2596aa
木村駿也 <kimurashunya@kimurashunyanoMacBook-Pro.local>
2023-08-15 16:19:02 +0900

    Add operator

---
+++ Operator.java
@@ -0,0 +1,8 @@
+public class Operator{
+  public static void main(String[]args){
+    String name = "太郎";
+    int age = 17;
+
+    System.out.println("初めまして、なまえは" + name + "です。" + age + "歳です。");
+  }
+}
