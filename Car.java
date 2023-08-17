5d9b774d203dfd9695acf8c13e73fb043fec22fd
木村駿也 <kimurashunya@kimurashunyanoMacBook-Pro.local>
2023-08-17 16:29:23 +0900

    Update car

--- Car.java
+++ Car.java
@@ -1,10 +1,59 @@
 public class Car{
  //モデルを表すメンバ変数model
- public String model;
+ private String model;
  //車高を表すメンバ変数height
- public int height;
+ private int height;
  //車幅を表すメンバ変数width
- public int width;
+ private int width;
  //燃料を表すメンバ変数fuel
- public int fuel;
+ private int fuel;
+
+  //モデルを出力する
+  public String getModel(){
+   return model;
+  }
+
+  //モデルを入力する
+  public void setModel(String model){
+   this.model = model;
+  }
+
+  //車高を出力する
+  public int getHeight(){
+   return height;
+  }
+
+  //車高を入力する
+  public void setHeight(int height){
+   this.height = height;
+  }
+
+  //車幅を出力する
+  public int getWidth(){
+   return width;
+  }
+
+  //車幅を入力する
+  public void setWidth(int width){
+   this.width = width;
+  }
+
+  //燃料を出力する
+  public int getFuel(){
+   return fuel;
+  }
+
+  //燃料を入力する
+  public void setFuel(int fuel){
+   this.fuel = fuel;
+  }
+
+  //各出力メソッドを使い、詳細を表示
+  public void show(){
+   System.out.println("*" + getModel() + "の詳細*");
+   System.out.println("モデル名:" + getModel());
+   System.out.println("車高 :" + getHeight());
+   System.out.println("車幅 :" + getWidth());
+   System.out.println("燃料容量:" + getFuel());
+  }
 }
--- CarApp.java
+++ CarApp.java
@@ -1,28 +1,24 @@
 public class CarApp {
-  public static void main(String[] args) {
-    Car car1 = new Car();
-    Car car2 = new Car();
+ public static void main(String[] args) {
+  Car car1 = new Car();
+  Car car2 = new Car();
 
-    car1.model = "車1号";
-    car1.height = 1400;
-    car1.width = 1600;
-    car1.fuel = 90;
+  //各メsetソッドを使い、car1のメンバ変数へ代入
+  car1.setModel("車1号");
+  car1.setHeight(1400);
+  car1.setWidth(1600);
+  car1.setFuel(90);
 
-    car2.model = "車2号";
-    car2.height = 1900;
-    car2.width = 1800;
-    car2.fuel = 100;
+  //各setメソッドを使い、car2のメンバ変数へ代入
+  car2.setModel("車2号");
+  car2.setHeight(1900);
+  car2.setWidth(1800);
+  car2.setFuel(100);
 
-    System.out.println("*" + car1.model + "の詳細*");
-    System.out.println("モデル名:" + car1.model);
-    System.out.println("車高　　:" + car1.height);
-    System.out.println("車幅　　:" + car1.width);
-    System.out.println("燃料容量:" + car1.fuel);
+  //car1の詳細を表示
+  car1.show();
 
-    System.out.println("*" + car2.model + "の詳細*");
-    System.out.println("モデル名:" + car2.model);
-    System.out.println("車高　　:" + car2.height);
-    System.out.println("車幅　　:" + car2.width);
-    System.out.println("燃料容量:" + car2.fuel);
-  }
+  //car2の詳細を表示
+  car2.show();
+ }
 }
