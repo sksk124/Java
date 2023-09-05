074547b60010d74e83bef1fc63b03b7455db1c51
木村駿也 <kimurashunya@kimurashunyanoMacBook-Pro.local>
2023-09-05 22:14:14 +0900

    Update JSample1_2

--- JSample1_2.java
+++ JSample1_2.java
@@ -1,5 +1,7 @@
 class JSample1_2{
  public static void main(String[] args){
-  System.out.println("こんにちは");
+  System.out.println('a');
+  System.out.println(0x0061);
+  System.out.println('\u0061');
  }
 }
