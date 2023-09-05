1df51131edde1125d903b219e20ba22637c2b570
木村駿也 <kimurashunya@kimurashunyanoMacBook-Pro.local>
2023-09-05 22:10:50 +0900

    Update JSample1_1

--- JSample1_1.java
+++ JSample1_1.java
@@ -1,5 +1,6 @@
 class JSample1_1{
  public static void main(String[] args){
-  System.out.println(System.getProperty("file.encoding"));
+  System.out.println('a');
+  System.out.println('あ');
  }
 }
