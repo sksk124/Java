c86d2c1afcb0eb43ad3e6c29eecdd4b53813b4d2
木村駿也 <kimurashunya@kimurashunyanoMacBook-Pro.local>
2023-09-13 22:37:54 +0900

    Update main

--- Main.java
+++ Main.java
@@ -1,16 +1,12 @@
 public class Main {
   public static void main(String[] args) {
-      int isHungry = 0;
-      String food = "おにぎり";
-      System.out.println("こんにちは");
-      if (isHungry == 0){
-          System.out.println("お腹がいっぱいです");
+      boolean tenki = true;
+      if (tenki == true) {
+          System.out.println("洗濯をします");
+          System.out.println("散歩にいきます");
       } else {
-          System.out.println("はらぺこです");
+          System.out.println("DVDを見ます");
+          System.out.println("寝ます");
       }
-      if (isHungry == 1) {
-          System.out.println(food + "をいただきます");
-      }
-      System.out.println("ごちそうさまでした");
   }
 }
