1efedce5f4e980a78c5e2e1e03dace26056e117c
木村駿也 <kimurashunya@kimurashunyanoMacBook-Pro.local>
2023-09-15 21:39:27 +0900

    Update main

--- Main.java
+++ Main.java
@@ -1,17 +1,28 @@
 public class Main {
   public static void main(String[] args) {
-      System.out.print("【数当てゲーム】");
-      int ans = new java.util.Random() .nextInt(10);
-      for (int i = 0; i < 5; i++) {
-          System.out.println("0~9の数字を入力してください");
-          int num = new java.util.Scanner(System.in) .nextInt();
-          if (ans == num) {
-              System.out.println("アタリ！");
-              break;
-          } else {
-              System.out.println("違います");
+      int[] seq = new int[10];
+      
+      //塩基配列をランダムに生成
+      for (int i = 0; i < seq.length; i++) {
+          seq[i] = new java.util.Random() .nextInt(4);
+      }
+      
+      //生成した塩基配列の記号を表示
+      for (int i = 0; i < seq.length; i++) {
+          switch (seq[i]) {
+              case 0:
+                  System.out.print("A ");
+                  break;
+              case 1:
+                  System.out.print("T ");
+                  break;
+              case 2:
+                  System.out.print("G ");
+                  break;
+              case 3:
+                  System.out.print("C ");
+                  break;
           }
       }
-      System.out.println("ゲームを終了します");
-   }
+  }
 }
